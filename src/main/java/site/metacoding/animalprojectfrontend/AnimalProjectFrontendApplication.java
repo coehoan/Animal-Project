3cdb715d733652e1e85831fef9cf14e596b4bad3
coehoan <coehoan@naver.com>
2022-04-06 15:10:04 +0900

    [최명신] Initial commit

---
+++ src/main/java/site/metacoding/animalprojectfrontend/AnimalProjectFrontendApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.animalprojectfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalProjectFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalProjectFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/animalprojectfrontend/AnimalProjectFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.animalprojectfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalProjectFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
