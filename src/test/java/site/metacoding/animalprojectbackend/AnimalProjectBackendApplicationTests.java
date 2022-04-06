14897cdefb04e4bf3d31651259c1bc6156583ad8
coehoan <coehoan@naver.com>
2022-04-06 14:44:31 +0900

    [최명신] Initial commit

---
+++ src/test/java/site/metacoding/animalprojectbackend/AnimalProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.animalprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
