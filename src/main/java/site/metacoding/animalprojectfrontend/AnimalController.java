aae0d5cc6dd642104aff66bd0d9efbcae0873ef9
coehoan <coehoan@naver.com>
2022-04-07 10:40:52 +0900

    [최명신] 메인페이지 생성완료

--- src/main/java/site/metacoding/animalprojectfrontend/AnimalController.java
+++ src/main/java/site/metacoding/animalprojectfrontend/AnimalController.java
@@ -1,8 +1,13 @@
 package site.metacoding.animalprojectfrontend;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AnimalController {
 
+    @GetMapping("/")
+    public String main() {
+        return "/main/mainForm";
+    }
 }
