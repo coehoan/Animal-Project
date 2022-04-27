1610f8e78789e2b56582f3b07a2522e878e0d223
coehoan <coehoan@naver.com>
2022-04-27 21:00:34 +0900

    [최명신] 보호소 조회 상세검색기능 구현완료

--- src/main/java/site/metacoding/animalprojectfrontend/domain/shelterde/ShelterDeRepository.java
+++ src/main/java/site/metacoding/animalprojectfrontend/domain/shelterde/ShelterDeRepository.java
@@ -1,7 +1,19 @@
 package site.metacoding.animalprojectfrontend.domain.shelterde;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ShelterDeRepository extends JpaRepository<ShelterDe, Integer> {
 
+    @Query(value = "SELECT * FROM shelterde WHERE orgNm =:addr", nativeQuery = true)
+    List<ShelterDe> findByAddr(@Param("addr") String addr);
+
+    @Query(value = "SELECT * FROM shelterde WHERE saveTrgtAnimal LIKE %:kind%", nativeQuery = true)
+    List<ShelterDe> findByKind(@Param("kind") String kind);
+
+    @Query(value = "SELECT * FROM shelterde WHERE orgNm =:addr AND saveTrgtAnimal LIKE %:kind%", nativeQuery = true)
+    List<ShelterDe> findByAddrAndKind(@Param("addr") String addr, @Param("kind") String kind);
 }
--- src/main/java/site/metacoding/animalprojectfrontend/web/ShelterController.java
+++ src/main/java/site/metacoding/animalprojectfrontend/web/ShelterController.java
@@ -1,5 +1,6 @@
 package site.metacoding.animalprojectfrontend.web;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
@@ -7,16 +8,42 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.animalprojectfrontend.domain.shelterde.ShelterDe;
 import site.metacoding.animalprojectfrontend.service.ShelterDeService;
+import site.metacoding.animalprojectfrontend.web.api.dto.shelterde.ShelterRespDto;
 
 @RequiredArgsConstructor
 @Controller
 public class ShelterController {
     private final ShelterDeService shelterDeService;
+
+    // 검색 공통 메서드
+    public void search(List<ShelterDe> shelterEntity, Model model) {
+        String REGEX = "[0-9]+";
+        List<ShelterRespDto> shelterList = new ArrayList<>();
+
+        for (ShelterDe shelterDe : shelterEntity) {
+            ShelterRespDto shelterRespDto = new ShelterRespDto();
+            shelterRespDto.setId(shelterDe.getId());
+            shelterRespDto.setOrgNm(shelterDe.getOrgNm());
+            shelterRespDto.setCareNm(shelterDe.getCareNm());
+            shelterRespDto.setCareAddr(shelterDe.getCareAddr());
+            shelterRespDto.setWeekOprStime(shelterDe.getWeekOprStime());
+            shelterRespDto.setWeekOprEtime(shelterDe.getWeekOprEtime());
+            shelterRespDto.setCloseDay(shelterDe.getCloseDay());
+            shelterRespDto.setCareTel(shelterDe.getCareTel());
+            shelterRespDto.setSaveTrgtAnimal(shelterDe.getSaveTrgtAnimal());
+            if (shelterDe.getCloseDay().matches(REGEX)) {
+                shelterRespDto.setCloseDay("정보없음");
+            }
+            shelterList.add(shelterRespDto);
+        }
+        model.addAttribute("shelterlistPage", shelterList);
+    }
 
     @GetMapping("/shelter/init-data")
     public @ResponseBody List<ShelterDe> initData(ShelterDe shelterDto, Model model) {
@@ -37,11 +64,38 @@ public class ShelterController {
         return "/animal/shelterList";
     }
 
-    @GetMapping("/test/shelter")
-    public @ResponseBody Page<ShelterDe> list() {
-        PageRequest pr = PageRequest.of(1, 3);
-        Page<ShelterDe> sheltersEntity = shelterDeService.전체보기(pr);
+    @GetMapping("/animal/shelterList/search")
+    public String search(
+            @RequestParam(value = "sido", required = false) String sido,
+            @RequestParam(value = "sigungu") String sigungu,
+            @RequestParam(value = "kind", required = false) String kind,
+            @RequestParam(value = "keyword", required = false) String keyword,
+            Model model) {
 
-        return sheltersEntity;
+        if (sido.equals("all") && sigungu.equals("all") && kind.equals("all")) {
+            return "redirect:/animal/shelterList";
+        }
+
+        // 지역, 시군구만 선택했을 때
+        if (sido != "all" && sigungu != "all" && kind.equals("all")) {
+            String addr = sido + " " + sigungu;
+            List<ShelterDe> shelterEntity = shelterDeService.지역별보기(addr);
+            search(shelterEntity, model);
+        }
+
+        // 종류만 선택했을 때
+        if (sido.equals("all") && sigungu.equals("all") && kind != "all") {
+            List<ShelterDe> shelterEntity = shelterDeService.종류별보기(kind);
+            search(shelterEntity, model);
+        }
+
+        // 지역, 시군구, 종류 모두 선택했을 때
+        if (sido != "all" && sigungu != "all" && kind != "all") {
+            String addr = sido + " " + sigungu;
+            List<ShelterDe> shelterEntity = shelterDeService.지역종류별보기(addr, kind);
+            search(shelterEntity, model);
+        }
+
+        return "/animal/shelterList";
     }
 }
